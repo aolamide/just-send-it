3e5c84b66de1e93d2293dd319f5176821ff1f469
olamide.aboyeji <olamide.aboyeji@konga.com>
2024-05-23 10:43:54 +0100

    init app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+    return (
+        <div className="bg-[#1E6D80] h-screen">
+            <div className="h-full w-full bg-no-repeat bg-cover bg-[url('./assets/splash-white.png')] flex flex-col justify-center items-center">
+                <img alt="Logo" className="w-35 h-35" src="/logo.svg" />
+                <h1 className="font-akaya font-semibold text-3xl">Just Send It</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App
+
---
+++ vite.config.js
@@ -0,0 +1,63 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'JustSendIt',
+          short_name: 'JustSendIt',
+          description: 'JustSendIt',
+          theme_color: '#ffffff',
+          screenshots: [
+            {
+              src: 'screenshot.png',
+              sizes: '384x840',
+              type: 'image/png',
+              form_factor: 'wide',
+              label: 'JustSendIt'
+            },
+            {
+              src: 'screenshot.png',
+              sizes: '384x840',
+              type: 'image/png',
+              form_factor: 'narrow',
+              label: 'JustSendIt'
+            }
+          ],
+          icons: [
+            {
+              src: 'pwa-64x64.png',
+              sizes: '64x64',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: 'maskable-icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+            }
+          ]
+        },
+        devOptions: {
+          enabled: true
+        }
+      })
+  ],
+})
+
