3b804e455716bb5f2b8a390e561c213b79a48695
olamide.aboyeji <olamide.aboyeji@konga.com>
2024-06-05 15:15:42 +0100

    navigate from splash screen automatically on app load

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import {useEffect} from "react";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 function App() {
+    const navigate = useNavigate();
     useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate('/home');
+        }, 1500);
 
-    }, []);
+        return () => clearTimeout(timer);
+    }, [navigate]);
     return (
-        <Link to="/home">
+        <>
             <div className="bg-primary h-screen">
                 <div
                     className="h-full w-full bg-no-repeat bg-cover bg-[url('./assets/splash-white.png')] flex flex-col justify-center items-center">
                     <img alt="Logo" className="w-35 h-35" src="/logo.svg"/>
                     <h1 className="font-akaya font-semibold text-3xl">Just Send It</h1>
+
+                    <svg className="animate-spin my-3 h-7 w-7 text-primary" xmlns="http://www.w3.org/2000/svg"
+                         fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
                 </div>
             </div>
-        </Link>
+        </>
 
     );
 }
